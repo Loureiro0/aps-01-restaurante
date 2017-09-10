19fbbbae5de5acb1ee4e03728c0fc5472dd40b96
vinicius da silva loureiro <viniciusloureiro1@gmail.com>
2017-09-10 13:23:33 -0300

    Adicionado a classe prato ao projeto.

---
+++ src/Prato.java
@@ -0,0 +1,28 @@
+
+public class Prato {
+	
+	private String nome;
+	private double preco;
+	private String descricao;
+	
+	
+	public Prato(String nome, double preco) {
+		super();
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+
+}
